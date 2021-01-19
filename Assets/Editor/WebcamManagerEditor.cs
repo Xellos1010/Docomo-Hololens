fd20df439b6ed5ba00cdeaac0bd777be48e52465
Evan McCall <e.mccallVR@Gmail.com>
2021-01-18 19:13:55 -0500

    Initial Commit

---
+++ Assets/Editor/WebcamManagerEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(WebcamManager))]
+public class WebcamManagerEditor : Editor
+{
+    WebcamManager instance;
+
+    void Start()
+    {
+        instance = (WebcamManager)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Use front-facing Camera"))
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<WebcamManager>();
+            instance.Action_useFrontCam(true);
+        }
+        if (GUILayout.Button("Start front-facing Camera"))
+        {
+            instance.Action_StartWebcam();
+        }
+        if (GUILayout.Button("Stop front-facing Camera"))
+        {
+            instance.Action_StopWebcam();
+        }
+    }
+}
+#endif
---
+++ Assets/FireEventOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FireEventOnLoad : MonoBehaviour
+{
+    public UnityEvent eventsToFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (eventsToFire != null)
+            eventsToFire.Invoke();
+    }
+}
